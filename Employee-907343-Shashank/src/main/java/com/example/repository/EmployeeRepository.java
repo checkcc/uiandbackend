11d4512f2ef5959ed65c9a0dcb0e18ad351f5688
Shashank Yadav <42097794+yshashanky@users.noreply.github.com>
2021-06-29 15:51:17 +0530

    Delete Employee-907343-Shashank directory

--- Employee-907343-Shashank/src/main/java/com/example/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.example.model.Employee;
-
-
-	public interface EmployeeRepository extends CrudRepository<Employee, Integer>
-	{
-	
-}
